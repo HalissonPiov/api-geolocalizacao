2efcde0c482c75fde54edeff4675d1fd9c5d7d35
Hálisson Piovezana <halissonpiovezana5@gmail.com>
2025-07-19 14:36:26 -0300

    feat: mapeamento das entidades

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
+from uuid import uuid4, UUID
+
+class Usuario(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    nome: str
+    email: str = Field(index=True, unique=True)
+    pontos: List["PontoGeografico"] = Relationship(back_populates="usuario")
+
+class PontoGeografico(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    latitude: float
+    longitude: float
+    descricao: str
+    usuario_id: UUID = Field(foreign_key="usuario.id")
+    usuario: Optional[Usuario] = Relationship(back_populates="pontos")
