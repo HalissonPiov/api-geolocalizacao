b4caa1a33be535aab3e5f8cd44ad9d76e9a4371a
Hálisson Piovezana <halissonpiovezana5@gmail.com>
2025-07-19 14:24:49 -0300

    feat: configuracoes do banco de dados

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException
+from uuid import UUID
+from .database import create_db
+from .crud import *
+
+app = FastAPI()
+create_db()
+
+@app.post("/AdicionarUsuario/")
+def adicionar_usuario(nome: str, email: str):
+    return criar_usuario(nome, email)
+
+@app.delete("/RemoverUsuario/")
+def deletar_usuario(email: str):
+    if remover_usuario(email):
+        return {"msg": "Usuário removido"}
+    raise HTTPException(status_code=404, detail="Usuário não encontrado")
+
+@app.post("/AdicionarPonto/")
+def adicionar_ponto_endpoint(usuario_id: UUID, latitude: float, longitude: float, descricao: str):
+    return adicionar_ponto(usuario_id, latitude, longitude, descricao)
+
+@app.delete("/RemoverPonto/")
+def deletar_ponto(ponto_id: UUID, usuario_id: UUID):
+    if remover_ponto(ponto_id, usuario_id):
+        return {"msg": "Ponto removido"}
+    raise HTTPException(status_code=404, detail="Ponto não encontrado")
+
+@app.get("/ListarPontosPorUsuario/")
+def listar_pontos(usuario_id: UUID):
+    return listar_pontos_usuario(usuario_id)
