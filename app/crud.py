400a002ba4449ce63302abc2affbfc1b1e8ae691
Hálisson Piovezana <halissonpiovezana5@gmail.com>
2025-07-20 14:59:56 -0300

    feat: rotas de atualizar e listar usuarios e de atualizar pontos

--- app/crud.py
+++ app/crud.py
@@ -21,6 +21,21 @@ def remover_usuario(email: str):
             return True
         return False
 
+def editar_usuario(email: str, novo_nome: str):
+    with get_session() as session:
+        usuario = session.exec(select(Usuario).where(Usuario.email == email)).first()
+        if usuario:
+            usuario.nome = novo_nome
+            session.add(usuario)
+            session.commit()
+            session.refresh(usuario)
+            return usuario
+        return None
+
+def buscar_usuario(email: str):
+    with get_session() as session:
+        return session.exec(select(Usuario).where(Usuario.email == email)).first()
+
 def adicionar_ponto(usuario_id: UUID, latitude: float, longitude: float, descricao: str):
     with get_session() as session:
         ponto = PontoGeografico(latitude=latitude, longitude=longitude, descricao=descricao, usuario_id=usuario_id)
@@ -39,6 +54,22 @@ def remover_ponto(ponto_id: UUID, usuario_id: UUID):
             session.commit()
             return True
         return False
+
+def atualizar_ponto(ponto_id: UUID, usuario_id: UUID, nova_latitude: float, nova_longitude: float, nova_descricao: str):
+    with get_session() as session:
+        ponto = session.exec(
+            select(PontoGeografico).where(PontoGeografico.id == ponto_id, PontoGeografico.usuario_id == usuario_id)
+        ).first()
+        if ponto:
+            ponto.latitude = nova_latitude
+            ponto.longitude = nova_longitude
+            ponto.descricao = nova_descricao
+            session.add(ponto)
+            session.commit()
+            session.refresh(ponto)
+            return ponto
+        return None
+
 
 def listar_pontos_usuario(usuario_id: UUID) -> List[PontoGeografico]:
     with get_session() as session:
--- app/main.py
+++ app/main.py
@@ -6,9 +6,24 @@ from .crud import *
 app = FastAPI()
 create_db()
 
+
 @app.post("/AdicionarUsuario/")
 def adicionar_usuario(nome: str, email: str):
     return criar_usuario(nome, email)
+
+@app.put("/AtualizarUsuario/")
+def atualizar_usuario(email: str, novo_nome: str):
+    usuario_atualizado = editar_usuario(email, novo_nome)
+    if usuario_atualizado:
+        return usuario_atualizado
+    raise HTTPException(status_code=404, detail="Usuário não encontrado")
+
+@app.get("/ListarUsuario/")
+def listar_usuario(email: str):
+    usuario = buscar_usuario(email)
+    if usuario:
+        return usuario
+    raise HTTPException(status_code=404, detail="Usuário não encontrado")
 
 @app.delete("/RemoverUsuario/") # Utilizar ID
 def deletar_usuario(email: str):
@@ -18,7 +33,11 @@ def deletar_usuario(email: str):
 
 @app.post("/AdicionarPonto/")
 def adicionar_ponto_endpoint(usuario_id: UUID, latitude: float, longitude: float, descricao: str):
-    return adicionar_ponto(usuario_id, latitude, longitude, descricao)
+    ponto = adicionar_ponto(usuario_id=usuario_id, latitude=latitude, longitude=longitude, descricao=descricao)
+    return {
+        "msg": "Ponto adicionado com sucesso",
+        "ponto": ponto
+    }
 
 @app.delete("/RemoverPonto/")
 def deletar_ponto(ponto_id: UUID, usuario_id: UUID):
@@ -26,6 +45,14 @@ def deletar_ponto(ponto_id: UUID, usuario_id: UUID):
         return {"msg": "Ponto removido"}
     raise HTTPException(status_code=404, detail="Ponto não encontrado")
 
+@app.put("/AtualizarPonto/")
+def atualizar_ponto_endpoint(ponto_id: UUID, usuario_id: UUID, nova_latitude: float, nova_longitude: float, nova_descricao: str):
+    ponto = atualizar_ponto(ponto_id, usuario_id, nova_latitude, nova_longitude, nova_descricao)
+    if ponto:
+        return ponto
+    raise HTTPException(status_code=404, detail="Ponto não encontrado")
+
 @app.get("/ListarPontosPorUsuario/")
 def listar_pontos(usuario_id: UUID):
     return listar_pontos_usuario(usuario_id)
+
